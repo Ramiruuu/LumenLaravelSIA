a31751b9761bf777fe05d7612e1de3b31607f2bf
Ramiruuu <oclaritremar25@gmail.com>
2025-04-02 14:37:07 +0800

    added routes

--- ddsbe1/routes/web.php
+++ ddsbe1/routes/web.php
@@ -16,16 +16,6 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->delete('/users/{id}', 'UserController@delete'); // Delete user
 });
 
-// Unsecured Routes with /api prefix (existing)
-$router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users', 'UserController@index'); // Get all users (http://localhost:8000/api/users)
-    $router->get('/users/{id}', 'UserController@show'); // Get user by ID (http://localhost:8000/api/users/{id})
-    $router->post('/users', 'UserController@add'); // Create a new user
-    $router->put('/users/{id}', 'UserController@update'); // Update user
-    $router->patch('/users/{id}', 'UserController@update'); // Partial user update
-    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
-});
-
 // New Unsecured Routes without /api prefix (new addition)
 $router->group([], function () use ($router) {
     $router->get('/users', 'UserController@index'); // Get all users (http://localhost:8000/users)
@@ -37,5 +27,5 @@ $router->group([], function () use ($router) {
 });
 
 // UserJob Routes
-$router->get('/userjob', 'UserJobController@index'); // Get all user jobs
+$router->get('/usersjob', 'UserJobController@index'); // Get all user jobs
 $router->get('/userjob/{id}', 'UserJobController@show'); // Get user job by ID
